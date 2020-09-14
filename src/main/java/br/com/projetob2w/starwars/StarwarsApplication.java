b18ae9dac59dd1669060771fbfffa5ebaf9b2b3d
isabeladutra <dutraisabela88@gmail.com>
2020-09-14 15:06:43 -0300

    correcao comparacap com o map

--- src/main/java/br/com/projetob2w/starwars/StarwarsApplication.java
+++ src/main/java/br/com/projetob2w/starwars/StarwarsApplication.java
@@ -28,8 +28,8 @@ import br.com.projetob2w.starwars.connection.*;
 @SpringBootApplication
 @RestController
 public class StarwarsApplication {
-	
-	@Autowired 
+
+	@Autowired
 	private ArquivoService arquivoService;
 	Map<String, Integer> mapaNomes = new HashMap<String, Integer>();
 
@@ -38,79 +38,85 @@ public class StarwarsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StarwarsApplication.class, args);
 	}
-	
+
 	@GetMapping("/planetas")
 	public List<Planeta> planetas(String name) {
 		List<Planeta> planet = arquivoService.findAll();
-		for(int i=0; i<=planet.size(); i++) {
-			
-			planet.get(i).setQuantidadeaparicoes(mapaNomes.get(planet.get(i).getNome()));
+		for (int i = 0; i < planet.size(); i++) {
+			String nome = planet.get(i).getNome();
+
+			if ( mapaNomes.get(nome) != null) {
+
+				planet.get(i).setQuantidadeaparicoes(mapaNomes.get(planet.get(i).getNome()));
+			} else {
+				planet.get(i).setQuantidadeaparicoes(0);
+			}
+
 		}
 
-	return planet;
+		return planet;
 	}
-	
+
 	@GetMapping("/buscarporid")
-	 public Planeta buscarPorId (@RequestParam(value = "id") String id){		
-		   return arquivoService.findById(id);   
-	   }
-	
+	public Planeta buscarPorId(@RequestParam(value = "id") String id) {
+		return arquivoService.findById(id);
+	}
+
 	@GetMapping("/adicionarplaneta")
-	 public String adicionarPlaneta (@RequestParam(value = "nome")   String nome, @RequestParam (value = "clima") String clima, @RequestParam (value = "terreno") String terreno){
-		   arquivoService.salvar (nome, clima, terreno);
-		   return "Sucesso!";
-	   }
-	
+	public String adicionarPlaneta(@RequestParam(value = "nome") String nome,
+			@RequestParam(value = "clima") String clima, @RequestParam(value = "terreno") String terreno) {
+		arquivoService.salvar(nome, clima, terreno);
+		return "Sucesso!";
+	}
+
 	@GetMapping("/buscarpornome")
-	 public Planeta buscarPorNome (@RequestParam(value = "nome")   String nome){
-		 return  arquivoService.buscarPorNome(nome); 
-		 
-	   }
-	
+	public Planeta buscarPorNome(@RequestParam(value = "nome") String nome) {
+		return arquivoService.buscarPorNome(nome);
+
+	}
+
 	@GetMapping("/removerplaneta")
-	 public String removerPlaneta (@RequestParam(value = "id")   String id){
-		   arquivoService.delete(id); 
-		   return "Removido com Sucesso!";
-	   }
-	
-	 @Bean
-	    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-	        return builder.build();
-	    }
-	 
+	public String removerPlaneta(@RequestParam(value = "id") String id) {
+		arquivoService.delete(id);
+		return "Removido com Sucesso!";
+	}
 
-	    @Bean
-	    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-	        return args -> {
-	            NextRes planet = restTemplate.getForObject(
-	            		"https://swapi.dev/api/planets/?page=2", NextRes.class);
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	            HttpEntity < String > entity = new HttpEntity < String > ("parameters", headers);
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
-	            ResponseEntity < NextRes> result = restTemplate.exchange("https://swapi.dev/api/planets/?page=2", HttpMethod.GET, entity,
-	                NextRes.class);
-	            
-	            while (result.getBody().next != null) {
-	            
-	        
-	            List<Result> listaderesult = planet.getResults();
-	            
-	            for(int i=0; i<listaderesult.size(); i++){
-	            Result obj = listaderesult.get(i);
-	            String nome = obj.getName();
-	            List<String> filmes = obj.getFilms();
-	            int quantidadeaparicoes = filmes.size();	           
-	            
-	            mapaNomes.put(nome, quantidadeaparicoes);
-	            
-	            
-	            }
-	            
-	            result = restTemplate.exchange(result.getBody().next.replaceAll("http", "https"), HttpMethod.GET, entity, NextRes.class); 
-	         
-	            log.info("Resultado da Chamada REST: " + planet.toString());
-	        };
-	    };
-	}}
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			NextRes planet = restTemplate.getForObject("https://swapi.dev/api/planets/?page=2", NextRes.class);
+			HttpHeaders headers = new HttpHeaders();
+			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+			HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
+			ResponseEntity<NextRes> result = restTemplate.exchange("https://swapi.dev/api/planets/?page=2",
+					HttpMethod.GET, entity, NextRes.class);
+
+			while (result.getBody().next != null) {
+
+				List<Result> listaderesult = planet.getResults();
+
+				for (int i = 0; i < listaderesult.size(); i++) {
+					Result obj = listaderesult.get(i);
+					String nome = obj.getName();
+					List<String> filmes = obj.getFilms();
+					int quantidadeaparicoes = filmes.size();
+
+					mapaNomes.put(nome, quantidadeaparicoes);
+
+				}
+
+				result = restTemplate.exchange(result.getBody().next.replaceAll("http", "https"), HttpMethod.GET,
+						entity, NextRes.class);
+
+				log.info("Resultado da Chamada REST: " + planet.toString());
+			}
+			;
+		};
+	}
+}
